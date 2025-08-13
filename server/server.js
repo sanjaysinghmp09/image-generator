65dd6be64953f0f7c3c78120e9feb705f5fcaed8
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-13 23:19:58 +0530

    add mongoDB to my project

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const PORT = process.env.PORT || 4000
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('API Working fine!')
+})
+
+app.listen(PORT, () => 
+  console.log(`Server is running on port ${PORT}`))
