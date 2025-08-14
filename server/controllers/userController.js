a22b07a14788b0dd032def784adc6e34e85f7ff4
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-14 09:28:17 +0530

    create user controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,40 +3,35 @@ import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken'
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await userModel.create({
-            name,
-            email,
-            password: hashedPassword
-        });
-        res.status(201).json({ message: "User registered successfully", user: newUser });
-    } catch (error) {
-        res.status(500).json({ message: "Error registering user", error });
-    }
-};
+        const { name, email, password } = req.body;
 
-const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await userModel.findOne({ email });
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
+        if (!name || !email || !password) {
+            return res.json({ success: false, message: "Please fill all the fields" });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(401).json({ message: "Invalid credentials" });
+        const salt = await bcrypt.genSalt(10);
+        const hanshedPassword = await bcrypt.hash(password, salt);
+
+        const userData = {
+            name, email, password: hashedPassword
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        res.status(200).json({ message: "Login successful", token });
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        res.json({ success: true, token, user: { name: user.name } });
+
     } catch (error) {
-        res.status(500).json({ message: "Error logging in", error });
+        console.log(error)
+        res.json({ success: false, message: "Internal server error" });
+
     }
+
 };
+
 
 export { registerUser, loginUser };
