0bf0a35ee1f31b9a793cc99fb76290b413a5d591
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-16 22:36:18 +0530

    IMAGE CONTROLLER

---
+++ server/controllers/imageController.js
@@ -0,0 +1,25 @@
+
+
+export const generateImage = async (req, res) => {
+    try {
+
+        const {userId, prompt} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if(!user || !prompt) {
+            return res.json({ success: false, message: "User not found or prompt is missing" });
+        }
+
+        if(user.creditBalance === 0 || userModel.creditBalance < 0 ) {
+            return res.json({ success: false , message: "Insufficient credit balance" , creditBalance: user.creditBalance });
+        }
+
+
+
+
+    } catch(error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+    }
+}
