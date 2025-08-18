e07682b6ff1bb198eb113b2256ce71771db24a00
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-18 10:25:22 +0530

    image user auth

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import { generateImage } from '../controllers/imageController';
+import userAuth from '../middlewares/userAuth';
 
+const imageRouter = express.Router();
+
+imageRouter.post('/generate-image', userAuth, generateImage);
+
+export default imageRouter;
