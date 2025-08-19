fdbf3dd65c5ae637cda0e91c946a7e89d5f1540d
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-19 20:56:34 +0530

    update login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -7,6 +7,12 @@ const Login = () => {
   const [state, setState] = useState("Login");
   const { setShowLogin } = useContext(AppContext);
 
+  const [name , setName] = useState("");
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+
+  
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
