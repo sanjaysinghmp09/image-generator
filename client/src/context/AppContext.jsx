14b11fd8e16e1ea60245c7170b580cf2359af861
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-19 20:47:24 +0530

    add backend url in front end step -2

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,8 +3,9 @@ import { createContext, useState } from "react";
 export const AppContext = createContext() 
 
 const AppContextProvider = (props) => { 
-    const [user, setUser] = useState(false);
-    const [showLogin, setShowLogin] = useState(null);
+    const [user, setUser] = useState(null);
+    const [showLogin, setShowLogin] = useState(false);
+    const [token, setToken] = useState(null);
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
