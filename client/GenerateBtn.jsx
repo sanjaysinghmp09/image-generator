19ab813bf9a12775a9ef5d67578c16d4c3edba73
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-20 19:30:44 +0530

    All Set

--- client/GenerateBtn.jsx
+++ client/GenerateBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { assets } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
