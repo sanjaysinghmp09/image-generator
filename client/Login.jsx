c49d935c449d85fc7a446f9d4b0ceef709f2134b
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-08-21 20:25:57 +0530

    All Set done 2

--- client/Login.jsx
+++ client/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState , useContext } from 'react';
 import { assets } from '../assets/assets';
-import { AppContext } from '../context/AppContext'
+import { AppContext } from '../context/AppContext';
 
 const Login = () => {
     
